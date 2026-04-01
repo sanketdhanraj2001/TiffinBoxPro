a8d408f2fd0d73add2ead55c366b892345588bec
SanketSalunke2001 <salunkesanket49@gmail.com>
2026-04-01 17:04:27 +0530

    added base class

---
+++ TiffinBox.Domain/Common/BaseEntity.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TiffinBox.Domain.Common
+{
+    public abstract class BaseEntity
+    {
+        public Guid Id { get;  set; } = Guid.NewGuid();
+        public DateTime CreatedAt { get; protected set; } = DateTime.UtcNow;
+        public DateTime? UpdatedAt { get; protected set; } // only child classes can modify it 
+        public bool IsDeleted { get; protected set; } = false;
+
+        protected void UpdateTimestamp() => UpdatedAt = DateTime.UtcNow;
+
+        public void SoftDelete()
+        {
+            IsDeleted = true;
+            UpdateTimestamp();
+        }
+    }
+}
